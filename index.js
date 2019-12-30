4bad43eaff62c667822d610c58d4f293a357e65c
Hannah Drury <hannah_drury@yahoo.com>
2019-12-30 02:51:54 +0000

    Done.

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function johnLennonFacts(array)
 {
   var i = 0;
   var ex = "!!!";
-  while(i <= array.length)
+  while(i < array.length) // was originally doing <= which caused it to go one over the length of the array, since the array starts at 0
   {
     array[i] = array[i].concat(ex);
     i++;
@@ -25,17 +25,15 @@ function johnLennonFacts(array)
   return array;
 }
 
-
-/*function iLoveTheBeatles(num)
+function iLoveTheBeatles(num)
 {
   var emptyArray2 = [];
  
   do
   {
-    emptyArray2[num] = "I love the Beatles!";
+    emptyArray2.push("I love the Beatles!"); // originally did emptyArray2[num] = "I love the...", but then figured it wanted it put in only once 
     num++;
   }
   while (num < 15);
  return emptyArray2;
 }
-*/

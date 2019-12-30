412f9bc355fe3ae0d612232d00bfdeb7fa7eca31
Hannah Drury <hannah_drury@yahoo.com>
2019-12-30 02:15:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // add solution here
+
+function theBeatlesPlay(array1, array2)
+{
+  var emptyArray = [];
+  var i = 0;
+  for (i; i < 4; i++)
+  {
+    emptyArray[i] = `${array1[i]} plays ${array2[i]}`;
+  }
+  
+return emptyArray;  
+}
+
+function johnLennonFacts(array)
+{
+  var i = 0;
+  var ex = "!!!";
+  while(i <= array.length)
+  {
+    array[i] = array[i].concat(ex);
+    i++;
+  }
+  
+  return array;
+}
+
+
+/*function iLoveTheBeatles(num)
+{
+  var emptyArray2 = [];
+ 
+  do
+  {
+    emptyArray2[num] = "I love the Beatles!";
+    num++;
+  }
+  while (num < 15);
+ return emptyArray2;
+}
+*/
